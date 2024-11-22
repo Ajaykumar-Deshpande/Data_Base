ed6639e67a7cce8952392a976f22fc340044e452
Ajaykumar-Deshpande <ajaydeshpandexworkz@gmail.com>
2024-11-22 16:06:28 +0530

    Collage Dtaabase Stored Into SQL

---
+++ Collage_Database/collagedatabase.py
@@ -0,0 +1,15 @@
+import mysql.connector
+conn=mysql.connector.connect(host="localhost",username="root",password="Ajay@787898",database="collage_database")
+
+cur=conn.cursor()
+q="insert into student(rollno,student_name,branch) values(%s,%s,)"
+cur.execute("create table student(rollno int not null,student_name varchar(20) not null, branch varchar(20)not null)") 
+
+q="insert into student(rollno,student_name,branch)values(%s,%s,%s)"
+v=[(102,"arjun","ece"),(103,"sachin","IS"),(104,"Charan","Mechenical")]
+try:
+    cur.executemany(q,v)
+    conn.commit()
+except:
+    conn.rollback()
+
