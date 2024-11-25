ec611cad3e1d961325ae96915e20b671cd8c1ebe
AjayKumar Deshpande <ajaydeshpandexworkz@gmail.com>
2024-11-26 00:32:11 +0530

    Update collagedatabase.py

--- Collage_Database/collagedatabase.py
+++ Collage_Database/collagedatabase.py
@@ -2,7 +2,7 @@ import mysql.connector
 conn=mysql.connector.connect(host="localhost",username="root",password="Ajay@787898",database="collage_database")
 
 cur=conn.cursor()
-q="insert into student(rollno,student_name,branch) values(%s,%s,)"
+q="insert into student(rollno,student_name,branch)"
 cur.execute("create table student(rollno int not null primary key,student_name varchar(20) not null, branch varchar(20)not null)") 
 
 q="insert into student(rollno,student_name,branch) values(%s,%s,%s)"
